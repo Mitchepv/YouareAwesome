9a9319006f0cacc8e745e4d6302db51418c4b636
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-01-15 19:46:05 -0500

    added pictures

--- YouareAwesome/ContentView.swift
+++ YouareAwesome/ContentView.swift
@@ -9,42 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = ""
-    @State private var imageString = ""
+    @State private var imageName = ""
   
     var body: some View {
         VStack {
             Spacer()
             
-            Image(systemName:imageString)
+            Image(imageName)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.orange)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
             
             Text(message)
                 .font(Font.largeTitle)
-                .fontWeight(.ultraLight)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
 
             
             Spacer()
             
-            
             Button("Press me!") {
                 let message1 = "You are Awesome!"
                 let message2 = " You are Great!"
-                let imageString1 = "hand.thumbsup"
-                let imageString2 = "sun.max.fill"
                 
                 message = ( message == message1 ? message2 : message1 )
-                imageString = ( imageString == imageString1 ? imageString2 : imageString1 )
-                
-//                if message == message1 {
-//                    message = message2
-//                    imageString = imageString2
-//                } else{
-//                    message = message1
-//                    imageString = imageString1
-//                }
-//
+                imageName = ( imageName == "image0" ? "image1" : "image0" )
+
             }
             .buttonStyle(.borderedProminent)
             .font(Font.title2)
